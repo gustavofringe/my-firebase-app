7f0ac8e9e7dfba4f733a1fbacb21cbf0f357a63a
Guillaume <dussartguillaume@orange.fr>
2017-09-01 09:33:20 +0200

    what i do suite install package form & sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AppComponent} from './app.component';
 
-import { AppComponent } from './app.component';
+export const firebaseConfig = {
+    apiKey: "AIzaSyD9mtQ8vsTFqp-tdVLBZgvk83RicYlTJHg",
+    authDomain: "angularfirebase-1cf3d.firebaseapp.com",
+    databaseURL: "https://angularfirebase-1cf3d.firebaseio.com",
+    projectId: "angularfirebase-1cf3d",
+    storageBucket: "angularfirebase-1cf3d.appspot.com",
+    messagingSenderId: "209746225879"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
